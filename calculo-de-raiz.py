f6bdb772716692c20a5b6469c7c96ad96c64b62d
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2024-03-20 20:46:31 -0300

    calculo de raiz

---
+++ calculo-de-raiz.py
@@ -0,0 +1,10 @@
+import math
+
+x1 = input("Digite o valor de x1: ")
+x2 = input("Digite o valor de x2: ")
+y1 = input("Digite o valor de y1: ")
+y2 = input("Digite o valor de y2: ")
+
+distancia = sqr(((x2-x1)**2)+((y2-y1)**2))
+
+print(f"O distancia enre os dois pontos é {distancia}")
