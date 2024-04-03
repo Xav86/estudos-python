8e8418f7ec6abf7f27702a2c949a5a858d249bd3
Xav86 <gustavo.g.2005@gmail.com>
2024-04-03 16:29:40 -0300

    feito exercicio 12

--- atividade-01/ex-12-converter-decimal.py
+++ atividade-01/ex-12-converter-decimal.py
@@ -2,5 +2,8 @@ print("------------Conversor de decimal-------------")
 
 decimal = int(input("Digite um numero decimal: "))
 
-#não feito :(
-# não sei como converter...
+binario = bin(decimal)
+octal = oct(decimal)
+hexadecimal = hex(decimal)
+
+print(f"o correspondente em hexa, binário e octal de {decimal} são respectivamente {hexadecimal}, {binario} e {octal}")
