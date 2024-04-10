d7708e1cd47ee7cce1eb031a7a840e293f903ffa
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2024-04-10 19:39:38 -0300

    ajuste em um exercicio :)

--- avaliação/Optimus-Prime-bem-vindo.py
+++ avaliação/Optimus-Prime-bem-vindo.py
@@ -2,7 +2,7 @@ print("----------Bem vindo Optimus Prime---------")
 
 nome = input("Digite seu nome: ")
 
-if nome == "Optimus Prime":
+if nome.title() == "Optimus Prime":
     print("Bem-vindo, voçê é um guerreiro de Cybertron")
 else:
     print(f"Bom dia {nome}")
