a2a3278d13a0f15c645735640ad1edd97c0fcdfa
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2024-04-10 20:48:42 -0300

    aprimorado programa

--- exercicios02/soma-numeros.py
+++ exercicios02/soma-numeros.py
@@ -1,10 +1,21 @@
 numero = 0
 soma = 0
+contagem = 0
+maior = -9999999999
+menor = 9999999999
 
 while (numero != -1):
     numero = int(input("Digite um numero: "))
+    contagem += 1
     soma = soma + numero
 
-soma += 1
+    if numero > maior:
+        maior = numero
 
-print(f"A soma dos numeros deu {soma}")
+    elif numero < menor:
+        menor = numero
+
+soma += 1
+media = soma/contagem
+
+print(f"A soma dos numeros deu {soma}, a média {media:.2}. Maior numero {maior} e menor numero {menor}")
